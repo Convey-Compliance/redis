f40b2332da9c30b307be52977ce2145b8119fc4c
Serhii Shemshur <serhiis@softwareplanet.uk.com>
2016-12-13 18:37:37 +0200

    Add outDit property for specifying output directory

--- build/build_internal.cmd
+++ build/build_internal.cmd
@@ -14,11 +14,11 @@ if "%2" == "x64" (
 call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\bin\vcvars32.bat"
 
 echo ### build hiredis(%config%, %platform_%) ###
-msbuild ..\msvs\hiredis\hiredis.vcxproj /p:Configuration=%config%;Platform=%platform_%
+msbuild ..\msvs\hiredis\hiredis.vcxproj /p:Configuration=%config%;Platform=%platform_%;OutDir=../../bin/%platform_%/%config%
 if errorlevel 1 goto :fail
 
 echo ### build Win32_Interop(%config%, %platform_%) ###
-msbuild ..\src\Win32_Interop\Win32_Interop.vcxproj /p:Configuration=%config%;Platform=%platform_%
+msbuild ..\src\Win32_Interop\Win32_Interop.vcxproj /p:Configuration=%config%;Platform=%platform_%;OutDir=../../bin/%platform_%/%config%
 if errorlevel 1 goto :fail
 goto :end
 
