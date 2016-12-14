4b31338bfa4c07bb067202323a96b004ed0c20fa
Serhii Shemshur <serhiis@softwareplanet.uk.com>
2016-12-14 17:08:24 +0200

    Add copy hiredis.h to internal storage

--- build/publish.cmd
+++ build/publish.cmd
@@ -13,9 +13,12 @@ if "%2" == "x64" (
 set copy_files=hiredis.lib Win32_Interop.lib
 set copy_opts=/ZB /X /TEE /njh /njs /ndl /nfl /nc /ns /np
 
-set target_dir=\\conveydev.com\files\dev\sdlc-ci\development\user\hiredis\%platform_%\%config%\lib
+set target_dir=\\conveydev.com\files\dev\sdlc-ci\development\user\hiredis
 
-robocopy ..\bin\%platform_%\%config%\ %target_dir% %copy_files% %copy_opts%
+robocopy ..\bin\%platform_%\%config%\ %target_dir%\%platform_%\%config%\lib %copy_files% %copy_opts%
+IF ERRORLEVEL 1 exit /B 1
+
+robocopy ..\deps\hiredis %target_dir% hiredis.h %copy_opts%
 IF ERRORLEVEL 1 exit /B 1
 
 exit /b 0
